c41e2ac10fc1c0bc530e82c3bf29b14019847d3b
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2019-06-24 23:34:42 +0100

    Setup state for component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,61 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Global } from "@emotion/core"
 import { ThemeProvider } from "emotion-theming"
 import GitHubButton from "react-github-btn"
 
 import globalStyles from "../globalStyles"
-import { lightTheme, darkTheme } from "../themes"
+import themes from "../themes"
 import SEO from "../components/Seo"
 import { Container } from "../components/Container"
 import { Content } from "../components/Content"
 import { Heading, StyledLink } from "../components/Elements"
 
-const IndexPage = () => (
-  <ThemeProvider theme={lightTheme}>
-    <Global styles={globalStyles} />
-    <SEO title="Home" />
-    <Container>
-      <Content>
-        <Heading>darkmodejs-demo</Heading>
-        <p>
-          A demonstration of how you can use the @assortment/darkmodejs npm
-          package to theme your website based on a user's Operating System
-          theme, thanks to the matchMedia API and prefers-color-scheme media
-          queries.
-        </p>
-        <p>
-          This example has been created with Emotion and the{" "}
-          <StyledLink to="https://emotion.sh/docs/emotion-theming">
-            emotion-theming
-          </StyledLink>{" "}
-          package. You can view the code for this on Github, or look at the
-          darkmodejs package with the links below
-        </p>
-        <StyledLink to="https://www.npmjs.com/package/@assortment/darkmodejs">
-          <img
-            src="https://img.shields.io/npm/v/@assortment/darkmodejs.svg"
-            alt="NPM: @assortment/darkmodejs"
-          />
-        </StyledLink>
-        &nbsp;&nbsp;
-        <GitHubButton
-          href="https://github.com/assortment/darkmodejs"
-          data-show-count="true"
-          aria-label="Star assortment/darkmodejs on GitHub"
-        >
-          Star
-        </GitHubButton>
-      </Content>
-    </Container>
-  </ThemeProvider>
-)
+const IndexPage = () => {
+  const [theme, setTheme] = useState("light")
+
+  useEffect(() => {
+    setTheme("dark")
+  }, [])
+
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <Global styles={globalStyles} />
+      <SEO title="Home" />
+      <Container>
+        <Content>
+          <Heading>darkmodejs-demo</Heading>
+          <p>
+            A demonstration of how you can use the @assortment/darkmodejs npm
+            package to theme your website based on a user's Operating System
+            theme, thanks to the matchMedia API and prefers-color-scheme media
+            queries.
+          </p>
+          <p>
+            This example has been created with Emotion and the{" "}
+            <StyledLink to="https://emotion.sh/docs/emotion-theming">
+              emotion-theming
+            </StyledLink>{" "}
+            package. You can view the code for this on Github, or look at the
+            darkmodejs package with the links below
+          </p>
+          <StyledLink to="https://www.npmjs.com/package/@assortment/darkmodejs">
+            <img
+              src="https://img.shields.io/npm/v/@assortment/darkmodejs.svg"
+              alt="NPM: @assortment/darkmodejs"
+            />
+          </StyledLink>
+          &nbsp;&nbsp;
+          <GitHubButton
+            href="https://github.com/assortment/darkmodejs"
+            data-show-count="true"
+            aria-label="Star assortment/darkmodejs on GitHub"
+          >
+            Star
+          </GitHubButton>
+        </Content>
+      </Container>
+    </ThemeProvider>
+  )
+}
 
 export default IndexPage
--- src/themes.js
+++ src/themes.js
@@ -1,21 +1,22 @@
-export const lightTheme = {
-  text: {
-    heading: "#000000",
-    body: "#777777",
+export default {
+  light: {
+    text: {
+      heading: "#000000",
+      body: "#777777",
+    },
+    background: {
+      backdrop: "#F7F7F7",
+      contentArea: "#EEEEEE",
+    },
   },
-  background: {
-    backdrop: "#F7F7F7",
-    contentArea: "#EEEEEE",
-  },
-}
-
-export const darkTheme = {
-  text: {
-    heading: "#FFF",
-    body: "#c2c2c2",
-  },
-  background: {
-    backdrop: "#100e17",
-    contentArea: "#16141f",
+  dark: {
+    text: {
+      heading: "#FFF",
+      body: "#c2c2c2",
+    },
+    background: {
+      backdrop: "#100e17",
+      contentArea: "#16141f",
+    },
   },
 }
