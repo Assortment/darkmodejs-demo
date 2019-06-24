f7f0c6c0d4d983c3852de4308aef1fc2cb9acb2c
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2019-06-24 23:20:41 +0100

    Add Emotion and Github Button packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `darkmodejs-demo`,
+        short_name: `darkmodejs-demo`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -27,8 +27,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        sourceMap: true,
+      },
+    },
   ],
 }
